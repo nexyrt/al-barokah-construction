85da6a3f8eb2e42dc2ddd12b5d3f21dc406c54dc
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-12-04 13:20:24 +0800

    icon

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,3 +1,3 @@
-<div class="flex aspect-square size-10 items-center justify-center rounded-md ">
+<div class="flex aspect-square size-12 items-center justify-center rounded-md ">
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
